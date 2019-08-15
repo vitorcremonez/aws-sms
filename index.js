91832c97786157bb99659ef77069e12742b4185c
Vitor Cremonez <vitorcremonez@gmail.com>
2019-08-15 13:44:54 -0300

    Installing minimist and configuring params

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 let AWS = require('aws-sdk');
+let argv = require('minimist')(process.argv.slice(2));
 require('dotenv').config();
 
-//console.log('Hello World!', process.env);
-console.log(AWS);
+function isParametersOkay({cellphone, message, subject}) {
+    let success = true;
+    if (!cellphone) {
+        console.error('Is missing parameters --cellphone');
+        success = false;
+    }
+    if (!message) {
+        console.error('Is missing parameters --message');
+        success = false;
+    }
+    if (!subject) {
+        console.error('Is missing parameters --subject');
+        success = false;
+    }
+    return success;
+}
+
+function main() {
+    const params = {
+        cellphone: argv['cellphone'],
+        message: argv['message'],
+        subject: argv['subject'],
+    };
+    if (isParametersOkay(params)) {
+        console.log('vai la');
+    }
+}
+
+main();
